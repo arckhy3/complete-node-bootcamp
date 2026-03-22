8a9d1d964cb56db21b915b99212ac2b50a1f63e0
arckhy <74829523+arckhy3@users.noreply.github.com>
2026-03-22 19:35:26 +0800

    refactor: clean up index.js and add Prettier configuration

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const http = require('http');
-// const url = require('url');
 const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
@@ -23,7 +22,7 @@ const replaceTemplate = require('./modules/replaceTemplate');
 //             console.log(data3);
 //             fs.writeFile('./starter/txt/final.txt',`${data2}\n${data3}`, 'utf-8', (err) => {
 //                 console.log('Your file has been written');
-//             }); 
+//             });
 //         });
 //     });
 // });
@@ -33,86 +32,98 @@ const replaceTemplate = require('./modules/replaceTemplate');
 //////////////////////////////////////////////////////////
 // SERVER
 
-const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
-const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8');
+const data = fs.readFileSync(
+  `${__dirname}/starter/dev-data/data.json`,
+  'utf-8',
+);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/starter/templates/template-overview.html`,
+  'utf-8',
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/starter/templates/template-product.html`,
+  'utf-8',
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/starter/templates/template-card.html`,
+  'utf-8',
+);
 const dataObj = JSON.parse(data);
 
 if (!dataObj[0].slug) {
-    const updateDataWithSlugs = dataObj.map(el => {
-        return {
-            ...el,
-            slug: slugify(el.productName, {lower:true})
-        }
-    });
+  const updateDataWithSlugs = dataObj.map((el) => {
+    return {
+      ...el,
+      slug: slugify(el.productName, { lower: true }),
+    };
+  });
 
-    const temp = fs.writeFileSync(
-        `${__dirname}/starter/dev-data/data.json`,
-        JSON.stringify(updateDataWithSlugs, null, 2)
-)
+  const temp = fs.writeFileSync(
+    `${__dirname}/starter/dev-data/data.json`,
+    JSON.stringify(updateDataWithSlugs, null, 2),
+  );
 }
 
 // console.log('Available product slugs:', updateDataWithSlugs.map(el => el.slug));
 
-const server = http.createServer((req,res)=>{
-    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
-    const pathname = parsedUrl.pathname;
+const server = http.createServer((req, res) => {
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = parsedUrl.pathname;
 
-    if(pathname === '/' || pathname === '/overview'){
-        res.writeHead(200, {'Content-type' : 'text/html'});
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+  } else if (pathname.startsWith('/product')) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const query = parsedUrl.searchParams;
+    let product;
+
+    if (pathname === '/product') {
+      // Support both ID and slug-based routing
+      if (query.has('id')) {
+        product = dataObj[query.get('id')];
+      } else if (query.has('slug')) {
+        product = dataObj.find((el) => el.slug === query.get('slug'));
+      }
+
+      if (product) {
+        const output = replaceTemplate(tempProduct, product);
         res.end(output);
-    }else if(pathname.startsWith('/product')){
-        res.writeHead(200, {'Content-type' : 'text/html'});
-        
-        const query = parsedUrl.searchParams;
-        let product;
-        
-        if (pathname === '/product') {
-            // Support both ID and slug-based routing
-            if (query.has('id')) {
-                product = dataObj[query.get('id')];
-            } else if (query.has('slug')) {
-                product = dataObj.find(el => el.slug === query.get('slug'));
-            }
-            
-            if (product) {
-                const output = replaceTemplate(tempProduct, product);
-                res.end(output);
-            } else {
-                // Product not found
-                res.writeHead(404, {'Content-type': 'text/html'});
-                res.end('<h1>Product not found</h1>');
-            }
-        } else {
-            const pathParts = pathname.split('/');
-            const slug = pathParts[2];
-            product = dataObj.find(el => el.slug === slug);
-            
-            if (product) {
-                const output = replaceTemplate(tempProduct, product);
-                res.end(output);
-            } else {
-                // Product not found
-                res.writeHead(404, {'Content-type': 'text/html'});
-                res.end('<h1>Product not found</h1>');
-            }
-        }
+      } else {
+        // Product not found
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end('<h1>Product not found</h1>');
+      }
+    } else {
+      const pathParts = pathname.split('/');
+      const slug = pathParts[2];
+      product = dataObj.find((el) => el.slug === slug);
+
+      if (product) {
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+      } else {
+        // Product not found
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end('<h1>Product not found</h1>');
+      }
     }
-    else if(pathname === '/api'){
-        res.writeHead(200, {'Content-type' : 'application/json'});
-        res.end(data);
-    }
-    else {
-        res.writeHead(404,{
-            'Content-type' : 'text-html',
-            'my-own-header' : 'hello-world'
-        });
-        res.end('<h1>Page not found<h1>');
-    }
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text-html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found<h1>');
+  }
 });
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
+  console.log('Listening to requests on port 8000');
 });
